66b82d5e2cc3fbbcfab15dc71bca9341026b015c
TechMussab <100826365+TechMussab@users.noreply.github.com>
2022-04-26 15:09:34 +0500

    change start

--- src/Home.js
+++ src/Home.js
@@ -14,11 +14,12 @@ class Home extends Component {
 	handleChange = (e) => this.setState({ url: e.target.value })
 
 	join = () => {
+		let url = '172.20.1.93'
 		if (this.state.url !== "") {
-			var url = this.state.url.split("/")
+			 url = this.state.url.split("/")
 			window.location.href = `/${url[url.length-1]}`
 		} else {
-			var url = Math.random().toString(36).substring(2, 7)
+			 url += Math.random().toString(36).substring(2, 7)
 			window.location.href = `/${url}`
 		}
 	}
@@ -26,18 +27,6 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
-					Source code: 
-					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
-						<GitHubIcon />
-					</IconButton>
-				</div>
-				
-				<div>
-					<h1 style={{ fontSize: "45px" }}>Video Meeting</h1>
-					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends.</p>
-				</div>
-
 				<div style={{
 					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
 					textAlign: "center", margin: "auto", marginTop: "100px"
@@ -45,7 +34,10 @@ class Home extends Component {
 					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
 					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
 					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
+					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Create meeting</p>
+					<Button variant="contained" color="primary" onClick={this.join} style={{ }}>Create</Button>
 				</div>
+
 			</div>
 		)
 	}
